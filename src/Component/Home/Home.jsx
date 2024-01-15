7c1aa225d5989065c64f133adcc2016540dca2dc
KevinRMO <romanokevin1992@gmail.com>
2024-01-15 15:17:50 +0100

    création des fichiers/dossiers et route

---
+++ src/Component/Home/Home.jsx
@@ -0,0 +1,13 @@
+import "./Home.css";
+import NavBar from "../Navbar/Navbar";
+import PostedAnnonce from "../PostedAnnonce/PostedAnnonce";
+import SearchBar from "../SearchBar/SearchBar";
+
+function Home() {
+  return (
+    <>
+      <NavBar />
+    </>
+  );
+}
+export default Home;
