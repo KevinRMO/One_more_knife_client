1e99fa2ad88fc724002f14ec8bd650a3e05569d2
KevinRMO <romanokevin1992@gmail.com>
2024-01-23 18:10:13 +0100

    Inscription candidat

--- src/Component/RegisterUser/RegisterUser.jsx
+++ src/Component/RegisterUser/RegisterUser.jsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import "./RegisterUser.css";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
@@ -10,6 +11,7 @@ import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Alert from "@mui/material/Alert";
 
 function Copyright(props) {
   return (
@@ -21,7 +23,7 @@ function Copyright(props) {
     >
       {"Copyright © "}
       <Link color="inherit" href="https://mui.com/">
-        Your Website
+        One More Knife
       </Link>{" "}
       {new Date().getFullYear()}
       {"."}
@@ -33,15 +35,12 @@ const defaultTheme = createTheme();
 
 function RegisterUser() {
   const navigate = useNavigate();
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get("email"),
-      password: data.get("password"),
-    });
-  };
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [passwordsMatch, setPasswordsMatch] = useState(true);
+  const [showSuccess, setShowSuccess] = useState(false);
+  const [showError, setShowError] = useState(false);
+  const [showCVError, setShowCVError] = useState(false);
 
   const navigateToHome = () => {
     navigate("/");
@@ -53,6 +52,74 @@ function RegisterUser() {
 
   const navigateToCompany = () => {
     navigate("/register-company");
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleConfirmPasswordChange = (event) => {
+    setConfirmPassword(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    // Vérifier si tous les champs sont remplis
+    const formElements = event.currentTarget.elements;
+    for (const element of formElements) {
+      if (element.required && element.value.trim() === "") {
+        setShowError(true);
+        setTimeout(() => {
+          setShowError(false);
+        }, 3000);
+        return;
+      }
+    }
+
+    if (password !== confirmPassword) {
+      console.error("Les mots de passe ne correspondent pas");
+      setPasswordsMatch(false);
+      return;
+    }
+    setPasswordsMatch(true);
+
+    // Vérifier si le fichier CV est au format PDF
+    const cvFile = event.currentTarget.querySelector("#cv_path");
+    if (cvFile && cvFile.files.length > 0) {
+      const allowedExtensions = ["pdf"];
+      const fileName = cvFile.files[0].name;
+      const fileExtension = fileName.split(".").pop().toLowerCase();
+      if (!allowedExtensions.includes(fileExtension)) {
+        console.error("Le fichier CV doit être au format PDF");
+        setShowError(true); // Afficher le message d'erreur général
+        setShowCVError(true); // Afficher le message d'erreur spécifique au CV
+        setTimeout(() => {
+          setShowError(false);
+          setShowCVError(false);
+        }, 3000);
+        return;
+      }
+    }
+
+    const data = new FormData(event.currentTarget);
+    try {
+      const response = await fetch("http://localhost:8000/api/register-user", {
+        method: "POST",
+        body: data,
+      });
+      if (response.ok) {
+        setShowSuccess(true);
+        setTimeout(() => {
+          setShowSuccess(false);
+          navigateToLogin();
+        }, 3000);
+      } else {
+        console.error("erreur");
+      }
+    } catch (error) {
+      console.error("erreur", error);
+    }
   };
 
   return (
@@ -88,19 +155,19 @@ function RegisterUser() {
                 <TextField
                   required
                   fullWidth
-                  id="lastName"
+                  id="lastname"
                   label="Nom"
-                  name="lastName"
+                  name="lastname"
                   autoComplete="family-name"
                 />
               </Grid>
               <Grid item xs={12} sm={6}>
                 <TextField
                   autoComplete="given-name"
-                  name="firstName"
+                  name="firstname"
                   required
                   fullWidth
-                  id="firstName"
+                  id="firstname"
                   label="Prénom"
                   autoFocus
                 />
@@ -112,6 +179,29 @@ function RegisterUser() {
                   name="birth_date"
                   type="date"
                   id="birth_date"
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  name="zip_code"
+                  required
+                  fullWidth
+                  type="text"
+                  pattern="[0-9]{5}"
+                  id="zip_code"
+                  label="Code postal"
+                  title="Entrez un code postal français valide (5 chiffres)"
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  name="city"
+                  type="text"
+                  id="city"
+                  label="Ville"
                 />
               </Grid>
               <Grid item xs={12} sm={6}>
@@ -151,6 +241,8 @@ function RegisterUser() {
                   label="Mot de passe"
                   type="password"
                   id="password"
+                  value={password}
+                  onChange={handlePasswordChange}
                 />
               </Grid>
               <Grid item xs={12}>
@@ -161,6 +253,11 @@ function RegisterUser() {
                   label="Confirmer mot de passe"
                   type="password"
                   id="confirmPassword"
+                  error={!passwordsMatch}
+                  helperText={
+                    !passwordsMatch && "Les mots de passe ne correspondent pas"
+                  }
+                  onChange={handleConfirmPasswordChange}
                 />
               </Grid>
             </Grid>
@@ -173,6 +270,21 @@ function RegisterUser() {
             >
               Inscription
             </Button>
+            {showSuccess && (
+              <Alert severity="success" onClose={() => setShowSuccess(false)}>
+                Votre compte a été créer, redirection vers la connexion
+              </Alert>
+            )}
+            {showError && (
+              <Alert severity="error" onClose={() => setShowError(false)}>
+                Veuillez remplir tous les champs obligatoires.
+              </Alert>
+            )}
+            {showCVError && (
+              <Alert severity="error" onClose={() => setShowCVError(false)}>
+                Le fichier CV doit être au format PDF.
+              </Alert>
+            )}
             <Grid container justifyContent="flex-end">
               <Grid item>
                 <Link
